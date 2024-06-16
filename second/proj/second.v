88acb6d18a7c6def9b63870e9e0faf0b263f9bb6
goodhumored <goodhumored@vk.com>
2024-06-16 22:57:17 +0300

    second lab fixed

--- second/proj/second.v
+++ second/proj/second.v
@@ -1,18 +1,17 @@
-module second(
-	input wire [2:0] x,
-	output reg [1:0] y
-);
-	always @* begin
-		case(x)
-			3'b000: y = 2'b11;
-			3'b001: y = 2'b00;
-			3'b010: y = 2'b11;
-			3'b011: y = 2'b01;
-			3'b100: y = 2'b01;
-			3'b101: y = 2'b10;
-			3'b110: y = 2'b10;
-			3'b111: y = 2'b01;
-			default: y = 2'b00;
-		endcase
-	end
+module multiplexer(input [1:0] selector, input [3:0] outputs, output reg out);
+  always @* 
+  begin
+    case(selector)
+      2'b00: out = outputs[0];
+      2'b01: out = outputs[1];
+      2'b10: out = outputs[2];
+      2'b11: out = outputs[3];
+      default: out = 1'b0;
+    endcase
+  end
+endmodule
+
+module second(input [2:0] x, output [1:0] y);
+  multiplexer multiplexer1({x[2], x[1]}, {~x[0], ~x[0], x[0], ~x[0]}, y[1]);
+  multiplexer multiplexer2({x[2], x[1]}, {~x[0], 1'b1, ~x[0], x[0]}, y[0]);
 endmodule
